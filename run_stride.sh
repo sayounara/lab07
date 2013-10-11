2a2be394a3a4b0f434243fd3191e23db1f7f7fff
Joel Sommers <jsommers@acm.org>
2013-10-11 08:13:36 -0400

    Modify range of stride

--- run_stride.sh
+++ run_stride.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for (( EXPO=3 ; EXPO<=28; EXPO+=1 )); do
+for (( EXPO=15 ; EXPO<=30; EXPO+=1 )); do
     (( SIZE=2**EXPO ))
     echo "# running stride with $SIZE"
     ./stride $SIZE
